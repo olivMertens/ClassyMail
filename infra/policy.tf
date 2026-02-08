6721d5ef406b26b5e534abb9b48ab4b40b2b898b
Olivier Mertens <olmertens@microsoft.com>
2026-02-08 08:05:09 +0100

    feat(infra): add SecurityControl and CostControl tags policy

--- infra/policy.tf
+++ infra/policy.tf
@@ -182,3 +182,134 @@ resource "azurerm_subscription_policy_remediation" "add_g2s_mandatory_tags_sub" 
 
   depends_on = [azurerm_role_assignment.add_g2s_mandatory_tags_role_sub]
 }
+
+# --- Security Control and Cost Control Tags Policy ---
+
+variable "security_cost_policy_enabled" {
+  type        = bool
+  description = "Enable the SecurityControl and CostControl tags auto-fill policy."
+  default     = true
+}
+
+resource "azurerm_policy_definition" "add_security_cost_tags" {
+  name         = "add-security-cost-control-tags"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Add Security Control and Cost Control Ignore Tags"
+  description  = "This policy automatically adds 'SecurityControl' and 'CostControl' tags with value 'ignore' to all resources except subscriptions, resource groups, deployments, and management groups."
+
+  metadata = jsonencode({
+    category = "Tags"
+    version  = "1.0.0"
+  })
+
+  parameters = jsonencode({})
+
+  policy_rule = jsonencode({
+    if = {
+      allOf = [
+        {
+          field = "type"
+          notIn = [
+            "Microsoft.Resources/subscriptions",
+            "Microsoft.Resources/resourceGroups",
+            "Microsoft.Resources/deployments",
+            "Microsoft.Management/managementGroups",
+          ]
+        },
+        {
+          anyOf = [
+            {
+              field  = "tags['SecurityControl']"
+              exists = "false"
+            },
+            {
+              field  = "tags['CostControl']"
+              exists = "false"
+            },
+          ]
+        },
+      ]
+    }
+    then = {
+      effect = "modify"
+      details = {
+        roleDefinitionIds = [
+          "/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c",
+        ]
+        operations = [
+          {
+            operation = "addOrReplace"
+            field     = "tags['SecurityControl']"
+            value     = "ignore"
+          },
+          {
+            operation = "addOrReplace"
+            field     = "tags['CostControl']"
+            value     = "ignore"
+          },
+        ]
+      }
+    }
+  })
+}
+
+resource "azurerm_resource_group_policy_assignment" "add_security_cost_tags_rg" {
+  count                = var.security_cost_policy_enabled && var.tag_policy_scope == "resource_group" ? 1 : 0
+  name                 = "add-security-cost-tags"
+  resource_group_id    = azurerm_resource_group.rg.id
+  policy_definition_id = azurerm_policy_definition.add_security_cost_tags.id
+  location             = var.location
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_subscription_policy_assignment" "add_security_cost_tags_sub" {
+  count                = var.security_cost_policy_enabled && var.tag_policy_scope == "subscription" ? 1 : 0
+  name                 = "add-security-cost-tags"
+  subscription_id      = data.azurerm_subscription.current.subscription_id
+  policy_definition_id = azurerm_policy_definition.add_security_cost_tags.id
+  location             = var.location
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "add_security_cost_tags_role_rg" {
+  count = var.security_cost_policy_enabled && var.tag_policy_scope == "resource_group" ? 1 : 0
+
+  scope              = local.tag_policy_rg_scope_id
+  role_definition_id = "/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"
+  principal_id       = azurerm_resource_group_policy_assignment.add_security_cost_tags_rg[0].identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "add_security_cost_tags_role_sub" {
+  count = var.security_cost_policy_enabled && var.tag_policy_scope == "subscription" ? 1 : 0
+
+  scope              = local.tag_policy_sub_scope_id
+  role_definition_id = "/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"
+  principal_id       = azurerm_subscription_policy_assignment.add_security_cost_tags_sub[0].identity[0].principal_id
+}
+
+resource "azurerm_resource_group_policy_remediation" "add_security_cost_tags_rg" {
+  count = var.security_cost_policy_enabled && var.tag_policy_scope == "resource_group" ? 1 : 0
+
+  name                 = "remediate-security-cost-tags"
+  resource_group_id    = azurerm_resource_group.rg.id
+  policy_assignment_id = azurerm_resource_group_policy_assignment.add_security_cost_tags_rg[0].id
+
+  depends_on = [azurerm_role_assignment.add_security_cost_tags_role_rg]
+}
+
+resource "azurerm_subscription_policy_remediation" "add_security_cost_tags_sub" {
+  count = var.security_cost_policy_enabled && var.tag_policy_scope == "subscription" ? 1 : 0
+
+  name                 = "remediate-security-cost-tags"
+  subscription_id      = data.azurerm_subscription.current.subscription_id
+  policy_assignment_id = azurerm_subscription_policy_assignment.add_security_cost_tags_sub[0].id
+
+  depends_on = [azurerm_role_assignment.add_security_cost_tags_role_sub]
+}
